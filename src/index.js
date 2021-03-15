f263390be2f982f33fba4e26f44cdaf52391a1a6
prig25 <5446068@gmail.com>
2021-03-15 03:30:38 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,79 @@
-
-// You should implement your task here.
-
+/*module.exports = function towelSort (matrix) {
+  let newmatrix=[];
+  let x=0;
+  let lngth=0;
+  if (!matrix || matrix.length == 0) {
+    return [];
+  } else {
+    matrix.map(
+      (mtrx)=>{
+        switch(x) {
+          case '0':
+            for (let i=0; i<mtrx.length; i++) {
+              newmatrix[lngth+i]=mtrx[i];              
+            }
+            x=1;
+            lngth = lngth + mtrx.length;
+            break;
+          case '1':
+            mtrx.reverse();
+            for (let i=0; i<mtrx.length; i++) {
+              newmatrix[lngth+i]=mtrx[i];              
+            }
+            x=0;
+            lngth = lngth + mtrx.length;
+            break;
+        }   
+      }
+    )
+  return newmatrix;
+  }
+}*/
+/*module.exports = function towelSort (matrix) {
+  let newmatrix=[];
+  let x=0;
+  let lngth=0;
+  if (!matrix || matrix.length == 0) {
+    return [];
+  } else {
+    matrix.map(
+      (mtrx)=>{
+        switch(x) {
+          case '0':
+            for (let i=0; i<mtrx.length; i++) {
+              newmatrix.push(mtrx[i]);             
+            }
+            x=1;
+            break;
+          case '1':
+            mtrx.reverse();
+            for (let i=0; i<mtrx.length; i++) {
+              newmatrix.push(mtrx[i]);             
+            }
+            x=0;
+            break;
+        }   
+      }
+    )
+  return newmatrix;
+  }
+}*/
 module.exports = function towelSort (matrix) {
-  return [];
+  let newmatrix=[];
+  let newmtrx=[];
+  if (!matrix || matrix.length == 0) {
+    return [];
+  } else {    
+      for (let i=0; i<matrix.length; i++) {
+        !(i%2) ? newmatrix.push(matrix[i]) : newmatrix.push(matrix[i].reverse());
+        }          
+      newmatrix.map(
+        (mtrx)=>{
+          for (let i=0; i<mtrx.length; i++){
+            newmtrx.push(mtrx[i]);
+          }
+        }
+      );
+    return newmtrx;
+    }
 }
